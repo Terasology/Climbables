f3a4e7a36ab2ca9bab0c3d1f7395994155fa3dde
darkweird <foxflameless@gmail.com>
2021-09-09 11:18:56 +0300

    feature(gestaltv7-events): replace ReceiveEvent with gestaltv7's ReceiveEvent

--- src/main/java/org/terasology/climbables/ClimbablesPlacingSystem.java
+++ src/main/java/org/terasology/climbables/ClimbablesPlacingSystem.java
@@ -9,7 +9,6 @@ import org.terasology.engine.audio.events.PlaySoundEvent;
 import org.terasology.engine.entitySystem.entity.EntityRef;
 import org.terasology.engine.entitySystem.event.EventPriority;
 import org.terasology.engine.entitySystem.event.Priority;
-import org.terasology.engine.entitySystem.event.ReceiveEvent;
 import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
 import org.terasology.engine.entitySystem.systems.RegisterMode;
 import org.terasology.engine.entitySystem.systems.RegisterSystem;
@@ -29,6 +28,7 @@ import org.terasology.engine.world.block.family.BlockFamily;
 import org.terasology.engine.world.block.family.BlockPlacementData;
 import org.terasology.engine.world.block.items.BlockItemComponent;
 import org.terasology.engine.world.block.items.OnBlockItemPlaced;
+import org.terasology.gestalt.entitysystem.event.ReceiveEvent;
 import org.terasology.module.inventory.systems.InventoryManager;
 import org.terasology.module.inventory.systems.InventoryUtils;
 
